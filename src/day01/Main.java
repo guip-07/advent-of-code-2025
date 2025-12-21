ceceed3edcf35e71dc0de056ce12edf78514bf6b
Guilherme Pombinho <g.pombinho@campus.fct.unl.pt>
2025-12-21 18:32:37 +0000

    Início

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -1,6 +1,19 @@
 package day01;
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+import java.util.Scanner;
+import java.io.*;
+//--
+public class Main{
+    public int DIAL_START_POSITION = 50;
+    public int COUNT = 0;
+    public char LEFT = 'L';
+    public char RIGHT = 'R';
+    //--
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new FileReader("input.txt"));
+        String test = in.next();
+        System.out.println(test);
+        in.close();
+
     }
 }
+
