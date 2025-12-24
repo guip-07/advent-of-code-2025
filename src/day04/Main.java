e7a88ef17d85af38bfecd7b3ec6a635d063e93d2
Guilherme Pombinho <g.pombinho@campus.fct.unl.pt>
2025-12-24 00:06:13 +0000

    FIM_02

--- src/day04/Main.java
+++ src/day04/Main.java
@@ -1,48 +1,73 @@
 package day04;
+
 import java.io.*;
 import java.util.Scanner;
+
 //--
 public class Main {
     public static int COUNT = 0;
     public static char[][] MAP = new char[137][137];
     public final static char ROLL = '@';
+
     //--
-    public static void fileReader(Scanner in){
-        for(int i = 0; i < 137; i++){
+    public static void fileReader(Scanner in) {
+        for (int i = 0; i < 137; i++) {
             String line = in.nextLine();
-            for(int j = 0; j < 137; j++){
+            for (int j = 0; j < 137; j++) {
                 MAP[i][j] = line.charAt(j);
             }
         }
     }
+
     //--
-    public static void fileProcess(){
-        for(int i = 0; i < 137; i++){
-            for(int j = 0; j < 137; j++){
-                if(MAP[i][j] == ROLL) verifyPosition(i,j);
+    public static void fileProcess() {
+        int removed;
+        do {
+            removed = 0;
+            boolean[][] toRemove = new boolean[137][137];
+
+            for (int i = 0; i < 137; i++) {
+                for (int j = 0; j < 137; j++) {
+                    if (MAP[i][j] == ROLL) {
+                        if (verifyPosition(i, j)) {
+                            toRemove[i][j] = true;
+                        }
+                    }
+                }
             }
-        }
+            for (int i = 0; i < 137; i++) {
+                for (int j = 0; j < 137; j++) {
+                    if (toRemove[i][j]) {
+                        MAP[i][j] = '.';
+                        COUNT++;
+                        removed++;
+                    }
+                }
+            }
+        } while (removed > 0);
     }
-    //--
-    public static void verifyPosition(int x, int y){
+
+    public static boolean verifyPosition(int x, int y) {
         int count = 0;
-        for(int i = -1; i <= 1; i++){
-            for(int j = -1; j <= 1; j++){
-                if(((x+i)>=0)&&((x+i)<=136)&&((y+j)>=0)&&((y+j)<=136)) {
-                    if (MAP[x + i][y + j] == ROLL) count++;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int nx = x + i;
+                int ny = y + j;
+                if (nx >= 0 && nx < 137 && ny >= 0 && ny < 137) {
+                    if (MAP[nx][ny] == ROLL) count++;
                 }
             }
         }
-        if(count <= 4) COUNT++;
+        return count <= 4;
     }
     //--
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new FileReader("src/day04/input.txt"));
         fileReader(in);
         fileProcess();
         System.out.println(COUNT);
         in.close();
         //RESPOSTA 1 - 1411
-        //RESPOSTA 2 -
+        //RESPOSTA 2 - 8557
     }
 }
